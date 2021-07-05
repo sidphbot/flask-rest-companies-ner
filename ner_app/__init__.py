8f10763957f531022ce9c5cc1fb748dcc6814bac
Sidharth Pal <sidharth.pal1992@gmail.com>
2021-07-05 02:13:04 +0200

    Initial Commit

---
+++ ner_app/__init__.py
@@ -0,0 +1,29 @@
+from .resouces.ScanText import ScanText
+
+from flask import Flask
+from flask_restful import Api
+
+
+def create_ner_app():
+    """
+    app factory for company name extraction REST API
+
+    :return app: flask app object
+    """
+
+    # initialize ner pipeline (pre-trained) - from hugging face transformers
+    # - may use custom ner pipeline object generation function with custom checkpoints
+    from transformers import pipeline
+    ner_pipeline = pipeline('ner')
+
+    # create flask app
+    app = Flask(__name__)
+    api = Api(app)
+    api.add_resource(ScanText, '/', resource_class_kwargs={'ner': ner_pipeline})
+
+    return app
+
+
+if __name__ == '__main__':
+    ner_app = create_ner_app()
+    ner_app.run(debug=False)
---
+++ ner_app/utils/__init__.py
@@ -0,0 +1,20 @@
+def words_to_companies(scores, words):
+    """ scans I-ORG word tokens to form complete company names and respective aggregated confidences
+    :param scores:[float] confidence scores of each word token
+    :param words:[str] word tokens
+
+    :return aggregated confidences:[float], joined and refined company names:[str]
+    """
+    names = []
+    confidences = []
+    lastname = ''
+    for i in range(len(words)):
+        if '##' in words[i] or words[i] == '-' or str(lastname[-1] if len(lastname) > 0 else '') == '-':
+            lastname = lastname + words[i].replace('#', '')
+            names[-1] = lastname
+            confidences[-1] = (confidences[-1] + float(scores[i])) / 2
+        else:
+            names.append(words[i])
+            lastname = words[i]
+            confidences.append(float(scores[i]))
+    return confidences, names
